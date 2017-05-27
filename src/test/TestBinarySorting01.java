842cef658a97464b8fa545d6d1526ae31be6129b
wojtek <ronin12@wp.pl>
2017-05-27 07:02:15 +0200

    g

---
+++ src/test/TestBinarySorting01.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.jetbrains.annotations.TestOnly;
+
+/**
+ * Created by Vic on 2017-05-25.
+ */
+public class TestBinarySorting01 {
+
+
+
+    @Test
+    void Test01(){
+
+    }
+}
